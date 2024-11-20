9665558325748f0b4cdf30efc822f272c3b1a9e4
Miles Wilson <mileswilsondc@gmail.com>
2024-11-20 22:24:40 +0000

    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-app.secret_key = 'your_development_secret_key'
+app.secret_key = os.getenv('SECRET_KEY', 'your_development_secret_key')
 
 DB_PATH = 'articles.db'
 
